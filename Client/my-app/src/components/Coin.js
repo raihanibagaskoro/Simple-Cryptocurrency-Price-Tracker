c458988e0caad6f8be12dd1598efa7fc1c202e14
raihanibagaskoro <raihanibagaskoro@gmail.com>
2024-01-24 12:25:03 +0700

    initial

---
+++ Client/my-app/src/components/Coin.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Coin.css";
+
+const Coin = ({
+    name,
+    symbol,
+    priceUsd,
+    changePercent24Hr,
+    marketCapUsd
+}) => {
+    const formattedPrice = Number(priceUsd).toFixed(2);
+    const formattedChange = Number(changePercent24Hr).toFixed(2);
+    const formattedMarketCap = Number(marketCapUsd).toLocaleString();
+
+    return (
+        <div>
+            <div className="coin-container">
+                <div className="coin-row">
+                    <div className="coin">
+                        <h1>{name}</h1>
+                        <p className="coin-symbol">{symbol}</p>
+                    </div>
+                    <div className="coin-data">
+                        <p className="coin-price">${formattedPrice}</p>
+                        {formattedChange < 0 ? (
+                            <p className="coin-percent red">{formattedChange}%</p>
+                        ) : (
+                            <p className="coin-percent green">{formattedChange}%</p>
+                        )}
+                        <p className="coin-marketcap">${formattedMarketCap}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coin
